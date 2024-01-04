4e746bdb85569032a1d6ac5968338a3f757c240b
nicholasbay <bay.yong.wei@gmail.com>
2024-01-04 18:07:19 +0800

    Create testMap3
    Adding map markers from file data

---
+++ Section 15 - Web Mapping App/test/testMap3.py
@@ -0,0 +1,21 @@
+# Adding markers from file data
+
+import folium
+import pandas
+
+data = pandas.read_csv('Section 15 - Web Mapping App/mapping_data/Volcanoes.csv')
+
+lat = list(data['LAT'])
+lon = list(data['LON'])
+names = list(data['NAME'])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6)
+
+fg = folium.FeatureGroup(name='Volcanoes')
+
+for lt, ln, name in zip(lat, lon, names):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=name, icon=folium.Icon(color='red')))
+
+map.add_child(fg)
+
+map.save('Section 15 - Web Mapping App/test/testMap3.html')
